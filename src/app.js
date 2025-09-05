5127ccbe22652dd0eecc8d763a8ed8c7f90d76d3
abhishal-ks <abhishalsharma2002@gmail.com>
2025-09-05 14:26:57 -0700

    Add subscription functionality

--- src/app.js
+++ src/app.js
@@ -22,6 +22,7 @@ import likeRouter from "./routes/like.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import tweetRouter from "./routes/tweet.routes.js";
 import playlistRouter from "./routes/playlist.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 
 //routes declaration
@@ -31,6 +32,7 @@ app.use("/api/v1/like", likeRouter)
 app.use("/api/v1/comment", commentRouter)
 app.use("/api/v1/tweet", tweetRouter)
 app.use("/api/v1/playlist", playlistRouter)
+app.use("/api/v1/subscriptions", subscriptionRouter)
 
 // http://localhost:8000/api/v1/users/register
 
